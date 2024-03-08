809b67b012d91dbbe00d17dce70efa6d562be577
Andrey Filipenkov <decapitator@ukr.net>
2024-03-08 11:02:14 +0500

    Update OSXGLView.mm

--- xbmc/windowing/osx/OpenGL/OSXGLView.mm
+++ xbmc/windowing/osx/OpenGL/OSXGLView.mm
@@ -38,8 +38,8 @@
       NSOpenGLPFAAccelerated,
       NSOpenGLPFADoubleBuffer,
       NSOpenGLPFAOpenGLProfile, NSOpenGLProfileVersion3_2Core,
-      //NSOpenGLPFAColorSize,     (NSOpenGLPixelFormatAttribute)32,
-      NSOpenGLPFAAlphaSize,     (NSOpenGLPixelFormatAttribute)8,
+      NSOpenGLPFAColorSize,     (NSOpenGLPixelFormatAttribute)32,
+      //NSOpenGLPFAAlphaSize,     (NSOpenGLPixelFormatAttribute)8,
       NSOpenGLPFADepthSize,     (NSOpenGLPixelFormatAttribute)24,
       (NSOpenGLPixelFormatAttribute)0
   };
